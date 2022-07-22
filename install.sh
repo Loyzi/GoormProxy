aeec4dc0c3037c5a5fd5f65da635d725ede2370c
Loyzi <107613868+Loyzi@users.noreply.github.com>
2022-07-22 18:03:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,10 @@ make -f Makefile.Linux
 mkdir -p /var/log/3proxy
 mkdir /etc/3proxy
 cp bin/3proxy /usr/bin/
-cd /etc/3proxy ; wget
+cd /etc/3proxy ; wget https://raw.githubusercontent.com/Loyzi/GoormProxy/main/3proxy.cfg
 cd ~ ; rm 0.9.3.tar.gz ; rm -rf 3proxy-0.9.3
 tmux new-session -s socks
-echo "/usr/bin/3proxy /etc/3proxy/3proxy.cfg"
+echo "Installation successful!"
+echo "Command to start proxy: /usr/bin/3proxy /etc/3proxy/3proxy.cfg"
+echo "Login without password"
+echo "Combination for detach: Ctrl+B+D"
